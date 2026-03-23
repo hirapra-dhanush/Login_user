d0ace1655b657369b58288a234128a97de2e586c
hirapra-dhanush <hirparadhanus640@gmail.com>
2026-03-23 13:50:49 +0530

    updat

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ const UserRoutes = require("./routes/user.routes")
 
 const app = express();
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
